1a17177464896892e0dee512610c7dc224e8af6a
Mohd Salim <saalim9965@gmail.com>
2024-07-13 20:16:34 +0530

    section 9: lecture 150 adding the new project component & a resusable input component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import { useState } from "react";
+import NewProject from "./components/NewProject";
 import ProjectsSidebar from "./components/ProjectsSidebar";
 
 function App() {
+  const [createNewProject, setCreateNewProject] = useState(true);
   return (
-    <main className="h-screen my-8">
+    <main className="h-screen my-8 flex gap-8">
       <ProjectsSidebar />
+      {createNewProject && <NewProject />}
     </main>
   );
 }
