5d74c6d4ce9544d593ca696e758a4db4e01005f7
Mohd Salim <saalim9965@gmail.com>
2024-07-14 14:04:06 +0530

    section 9: lecture 156 validating user input and showing an Error Modal via useImperativeHandle

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,7 +1,10 @@
 import { useRef } from "react";
 import Input from "./Input";
+import Modal from "./Modal";
 
 export default function NewProject({ onAdd }) {
+  const modal = useRef();
+
   const title = useRef();
   const description = useRef();
   const dueDate = useRef();
@@ -13,6 +16,15 @@ export default function NewProject({ onAdd }) {
 
     // validation...
 
+    if (
+      enteredTitle.trim() === "" ||
+      enteredDescription.trim() === "" ||
+      enteredDueDate.trim() === ""
+    ) {
+      modal.current.open();
+      return;
+    }
+
     onAdd({
       title: enteredTitle,
       description: enteredDescription,
@@ -20,27 +32,34 @@ export default function NewProject({ onAdd }) {
     });
   }
   return (
-    <div className="w-[35rem] mt-16">
-      <menu className="flex items-center justify-end gap-4 my-4">
-        <li>
-          <button className="text-stone-800 hover:text-stone-950">
-            Cancel
-          </button>
-        </li>
-        <li>
-          <button
-            className="bg-stone-800 text-stone-50 hover:bg-stone-950 px-6 py-2 rounded-md"
-            onClick={handleSaveNewProject}
-          >
-            Save
-          </button>
-        </li>
-      </menu>
-      <div>
-        <Input label="Title" ref={title} type="text" />
-        <Input label="Description" textArea ref={description} />
-        <Input label="Due Date" ref={dueDate} type="date" />
+    <>
+      <Modal ref={modal} buttonCaption="Okay">
+        <h2>Invalid Input</h2>
+        <p>Oops ... looks like you forgot to enter a value</p>
+        <p>Please make sure you provide a valid value for every input field.</p>
+      </Modal>
+      <div className="w-[35rem] mt-16">
+        <menu className="flex items-center justify-end gap-4 my-4">
+          <li>
+            <button className="text-stone-800 hover:text-stone-950">
+              Cancel
+            </button>
+          </li>
+          <li>
+            <button
+              className="bg-stone-800 text-stone-50 hover:bg-stone-950 px-6 py-2 rounded-md"
+              onClick={handleSaveNewProject}
+            >
+              Save
+            </button>
+          </li>
+        </menu>
+        <div>
+          <Input label="Title" ref={title} type="text" />
+          <Input label="Description" textArea ref={description} />
+          <Input label="Due Date" ref={dueDate} type="date" />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
